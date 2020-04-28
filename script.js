bb30a116ffe39b335e9eab3e9f9fce2d5f7abdaf
shhr3y <shhr3y@github.com>
2020-04-29 00:28:15 +0530

    first commit

--- script.js
+++ script.js
@@ -4,37 +4,28 @@
  * Update the output text as you type in the textarea
  */
 function updateText(){
-  let text = document.getElementById("text-input").value;
-  document.getElementById('text-output').innerText = text;
+  
 }
 
 /**
  * Toggle the bold class for the output text
  */
 function makeBold(elem){
-  elem.classList.toggle('active');
-  document.getElementById('text-output').classList.toggle('bold');
+  
 }
 
 /**
  * Toggle the italic class for the output text
  */
 function makeItalic(elem){
-  elem.classList.toggle('active');
-  document.getElementById('text-output').classList.toggle('italic');
+  
 }
 
 /**
  * Toggle the underline class for the output text
  */
 function makeUnderline(elem){
-    elem.classList.toggle('active');
-    let output = document.getElementById('text-output');
-    if(output.classList.contains('underline')){
-      output.classList.remove('underline');
-    } else {
-      output.classList.add('underline');
-    }
+   
 }
 
 /**
@@ -42,10 +33,5 @@ function makeUnderline(elem){
  * Toggle the active state for the align butttons
  */
 function alignText(elem, alignType){
-  document.getElementById('text-output').style.textAlign = alignType;
-  let alignButtons = document.getElementsByClassName('align');
-  for(let i = 0; i < alignButtons.length; i++ ){
-    alignButtons[i].classList.remove('active');
-  }
-  elem.classList.toggle('active');
+ 
 }
